49c1007f6f55114beb2d451ac03b3355aa9999a0
yordanluturyalii <yordanluturyali@gmail.com>
2025-01-15 21:10:25 +0700

    feat: add basic exception :goal_net:

--- exceptions/error.go
+++ exceptions/error.go
@@ -1,1 +1,132 @@
 package exceptions
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"github.com/go-playground/validator"
+)
+
+type GlobalError interface {
+	Error() string
+	GetCode() int
+}
+
+type HttpBadRequestError struct {
+	Msg  string
+	Code int
+}
+
+func NewHttpBadRequestError(msg string) HttpBadRequestError {
+	return HttpBadRequestError{Msg: msg, Code: http.StatusBadRequest}
+}
+
+func (e HttpBadRequestError) Error() string {
+	return e.Msg
+}
+
+func (e HttpBadRequestError) GetCode() int {
+	return e.Code
+}
+
+type HttpNotFoundError struct {
+	Msg  string
+	Code int
+}
+
+func NewHttpNotFoundError(msg string) HttpNotFoundError {
+	return HttpNotFoundError{Msg: msg, Code: http.StatusNotFound}
+}
+
+func (e HttpNotFoundError) Error() string {
+	return e.Msg
+}
+
+func (e HttpNotFoundError) GetCode() int {
+	return e.Code
+}
+
+type HttpForbiddenError struct {
+	Msg  string
+	Code int
+}
+
+func NewHttpForbiddenError() HttpForbiddenError {
+	return HttpForbiddenError{Msg: "Forbidden", Code: http.StatusForbidden}
+}
+
+func (e HttpForbiddenError) Error() string {
+	return e.Msg
+}
+
+func (e HttpForbiddenError) GetCode() int {
+	return e.Code
+}
+
+type UnauthorizedError struct {
+	Msg  string
+	Code int
+}
+
+func NewUnauthorizedError(msg string) UnauthorizedError {
+	return UnauthorizedError{Msg: msg, Code: http.StatusUnauthorized}
+}
+
+func (e UnauthorizedError) Error() string {
+	return e.Msg
+}
+
+func (e UnauthorizedError) GetCode() int {
+	return e.Code
+}
+
+type HttpInternalServerError struct {
+	Msg  string
+	Code int
+}
+
+func NewHttpInternalServerError() HttpInternalServerError {
+	return HttpInternalServerError{Msg: "Internal Server Error", Code: http.StatusInternalServerError}
+}
+
+func (e HttpInternalServerError) Error() string {
+	return e.Msg
+}
+
+func (e HttpInternalServerError) GetCode() int {
+	return e.Code
+}
+
+type FailedValidation struct {
+	Msg  string
+	Code int
+	Errors map[string]interface{}
+}
+
+func NewFailedValidation(obj interface{}, err validator.ValidationErrors) FailedValidation {
+	objRef := reflect.TypeOf(obj)
+	errors := make(map[string]interface{})
+
+	for _, fieldError := range err {
+		fieldName := fieldError.Field()
+
+		if f, ok := objRef.Elem().FieldByName(fieldName); ok {
+			jsonTag := f.Tag.Get("json")
+			if jsonTag != "" {
+				fieldName = jsonTag
+			}
+		}
+
+		errors[fieldName] = fmt.Sprintf("%s", fieldError.Tag())
+	}
+
+	return FailedValidation{Msg: "Failed Validation", Code: http.StatusUnprocessableEntity, Errors: errors}
+}
+
+func (e FailedValidation) Error() string {
+	return e.Msg
+}
+
+func (e FailedValidation) GetCode() int {
+	return e.Code
+}
--- go.mod
+++ go.mod
@@ -2,12 +2,18 @@ module yordanluturyali/golang-auth-rest
 
 go 1.23.4
 
-require github.com/gofiber/fiber/v2 v2.52.6
+require (
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/gofiber/fiber/v2 v2.52.6
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
@@ -16,4 +22,5 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
